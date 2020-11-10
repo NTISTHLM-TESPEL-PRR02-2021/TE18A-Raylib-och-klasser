e1128b6f74480a81c39e9d255c6041054f4ffb56
Mikael <mikael.bergstrom@ga.ntig.se>
2020-11-10 13:25:43 +0100

    Tog bort ett par onödiga tomma rader =)

--- GameEngine/Program.cs
+++ GameEngine/Program.cs
@@ -11,8 +11,6 @@ namespace GameEngine
       Raylib.InitWindow(800, 600, "Game engine");
       Raylib.SetTargetFPS(60);
 
-      
-
       Paddle leftPaddle = new Paddle(10, 275, KeyboardKey.KEY_W, KeyboardKey.KEY_S);
       Paddle rightPaddle = new Paddle(770, 275, KeyboardKey.KEY_UP, KeyboardKey.KEY_DOWN);
 
