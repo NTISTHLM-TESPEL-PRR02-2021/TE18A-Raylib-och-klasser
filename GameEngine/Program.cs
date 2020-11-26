1c61c6c55f9122c3283143676bae704dc53b4c25
Mikael <mikael.bergstrom@ga.ntig.se>
2020-11-26 15:32:43 +0100

    Gamestates & enums

--- GameEngine/Program.cs
+++ GameEngine/Program.cs
@@ -6,6 +6,13 @@ namespace GameEngine
 {
   class Program
   {
+
+    enum GameScreens {
+      Start,
+      Game,
+      GameOver
+    }
+
     static void Main(string[] args)
     {
       Raylib.InitWindow(800, 600, "Game engine");
@@ -16,14 +23,43 @@ namespace GameEngine
 
       Ball ball = new Ball();
 
+      GameScreens screen = GameScreens.Start;
+
       while (!Raylib.WindowShouldClose())
       {
-        GameObject.UpdateAll();
+
+        // LOGIK
+        if (screen == GameScreens.Start)
+        {
+          if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+          {
+            screen = GameScreens.Game;
+          }
+        }
+        else if (screen == GameScreens.Game)
+        {
+          GameObject.UpdateAll();
+        }
+
+        
         // ball.Update();
 
-
+        // GRAFIK
         Raylib.BeginDrawing();
-        Raylib.ClearBackground(Color.GOLD);
+
+        if (screen == GameScreens.Start)
+        {
+          Raylib.ClearBackground(Color.GRAY);
+          Raylib.DrawText("Press ENTER to start", 10, 10, 64, Color.BLACK);
+        }
+        else if (screen == GameScreens.Game)
+        {
+          Raylib.ClearBackground(Color.GOLD);
+          GameObject.DrawAll();
+
+        }
+
+        
 
         // if (Raylib.CheckCollisionRecs(ballRect, ball2Rect))
         // {
@@ -34,7 +70,7 @@ namespace GameEngine
         //   Raylib.DrawText("NEJ", 10, 500, 64, Color.BLACK);
         // }
 
-        GameObject.DrawAll();
+        
 
         Raylib.EndDrawing();
       }
